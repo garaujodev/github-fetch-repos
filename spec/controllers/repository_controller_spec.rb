def8d8a6a6ce2adf45c61b3c18cbe6718d806b5f
Gustavo Araujo <garaujodev@gmail.com>
2023-07-21 11:40:39 -0300

    Add new test scenario to `repository_controller_spec`

--- spec/controllers/repository_controller_spec.rb
+++ spec/controllers/repository_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RepositoryController, type: :controller do
   describe 'POST #create' do
     it 'enqueues RepositoryJob with the provided username' do
-      username = 'test_user'
+      username = 'github'
 
       expect do
         post :create, params: { username: username }
@@ -13,8 +13,16 @@ RSpec.describe RepositoryController, type: :controller do
     end
 
     it 'returns a successful response' do
-      post :create, params: { username: 'test_user' }
+      post :create, params: { username: 'github' }
       expect(response).to have_http_status(:success)
+    end
+
+    it 'does not enqueue RepositoryJob and returns a not found response if user not exists' do
+      expect do
+        post :create, params: { username: 'this_username_doesnt_exist' }
+      end.not_to change(RepositoryJob.jobs, :size)
+
+      expect(response).to have_http_status(:not_found)
     end
 
     it 'does not enqueue RepositoryJob and returns a bad request response if username is not provided' do
