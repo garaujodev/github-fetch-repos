302f1bed7f7fad0b04a4751c2321fc31baa7f77b
Gustavo Araujo <garaujodev@gmail.com>
2023-07-21 11:21:38 -0300

    Return `404` status code when username doesnt exists

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -3,11 +3,15 @@ class RepositoryController < ApplicationController
     username = params[:username]
 
     if username.present?
-      RepositoryJob.perform_async(username)
-      head :no_content # Retorna o status code 204 (No Content) se a execução ocorrer normalmente
+      if GitHub::User.call(username)
+        RepositoryJob.perform_async(username)
+
+        head :no_content # Retorna o status code 204 (No Content) se a execução ocorrer normalmente
+      else
+        head :not_found # Retorna o status code 404 (Not Found) se o usuario não existir no GitHub
+      end
     else
       head :bad_request # Retorna o status code 400 (Bad Request) se o parâmetro "username" não estiver presente
     end
   end
 end
-
